e5e6b9fd8c294f2a519f644fb6a65deb89bd5c44
ugoryntsev <z-gys@yandex.ru>
2019-05-21 18:51:49 +0300

    add homework

---
+++ src/ru/mobiledimension/study/day1/Homework2.kt
@@ -0,0 +1,7 @@
+package ru.mobiledimension.study.day1
+
+/*
+   Чтобы снимать видео я купил комп стоимостью 200к и квадрокоптер за 57к.
+   Квадрокоптер я 2 раза разбил и его ремонт вышел 9880
+   Сколько стоит одно видео на моем канале: https://www.youtube.com/channel/UC1z9oTxNmllOO_IDbnxxtzg
+ */
